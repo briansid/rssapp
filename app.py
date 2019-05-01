dff201e9569f0d656f5bf0c3a70541ebd73d40ac
pd-brainsid <brainsid@alterpost.org>
2019-05-01 10:15:12 +0300

    screen manager added

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 
 
-class StartPage(AnchorLayout):
-    def subscription(self, *args):
-        hockey_subscription = self.ids['football_subscription']
-        print(dir(hockey_subscription))
+class FirstScreen(Screen):
+    pass
+
+
+class SecondScreen(Screen):
+    pass
+
+
+class MyScreenManager(ScreenManager):
+    pass
 
 
 class PMApp(App):
     def build(self):
-        return StartPage()
+        return MyScreenManager()
 
 
 if __name__ == '__main__':
