1d76aaad9a3e94abdd063353e37747fb83efd34b
pd-brainsid <brainsid@alterpost.org>
2019-05-02 14:18:19 +0300

    publish date added

--- app.py
+++ app.py
@@ -32,9 +32,35 @@ class MyGrid(GridLayout):
         # hockey = self.ids['_hockey']
         # print(hockey.state)
         # if hockey.state == 'down':
+
         for feed in self.feeds['entries']:
-            self.add_widget(Label(text=feed['title'], text_size=(self.width, None), size_hint_y=None, height=100))
-            self.add_widget(AsyncImage(source=feed['media_content'][0]['url'], size_hint=(None,None), size=(100,100)))
+            pd = datetime.strptime(feed['published'], '%Y-%m-%dT%H:%M:%S+00:00')
+            pd = datetime.now() - pd
+            pd = int(pd.total_seconds())
+
+            if pd <= 60:
+                pd = '%d секунд назад' % pd
+
+            elif pd <= 60*60:
+                pd = '%d минут назад' % pd/60
+
+            elif pd <= 60*60*24:
+                pd = pd/60/60
+                if pd == 1 or pd == 21:
+                    pd = '%d час назад' % pd
+                elif 5 > pd > 1 or pd > 21:
+                    pd = '%d часа назад' % pd
+                else:
+                    pd = '%d часов назад' % pd
+            else:
+                pd = '%d день назад' % (pd/60/60/24)
+
+            text = feed['title']
+            text += '\n'
+            text += pd
+
+            self.add_widget(Label(text=text, font_size=20, text_size=(self.width*3, None), pos_hint={'x': 0, 'top': 1}))
+            self.add_widget(AsyncImage(source=feed['media_content'][0]['url'], size_hint_y=None, height=150))
 
 
 class MyScreenManager(ScreenManager):
